d06dbc51889ed945efef17236f93440c1b29df74
David Meiklejohn <david@gooligum.com.au>
2015-11-11 17:15:29 +1100

    removed debug print statements

--- ip_disp.py
+++ ip_disp.py
@@ -5,7 +5,7 @@
 # Written by S G OBrien  April - June 2015.
 # modified by David Meiklejohn (Gooligum Electronics)
 #
-#   v1.1    23/10/15
+#   v1.2    11/11/15
 #
 # THIS CODE CANNOT BE USED OR REPRODUCED WITHOUT PERMISSION FROM THE AUTHORS
 # COPYRIGHT 2015.
@@ -23,30 +23,22 @@ display = joeyBoard()
 print "IP Address Display 4 digit 7 segment display module"
 
 # get first valid IP address line from ifconfig
-address = os.popen('sudo ifconfig | grep "inet addr" | grep -v 127.0.0.1').readline()
+address = os.popen('ifconfig | grep "inet addr" | grep -v 127.0.0.1').readline()
 
 # extract the address characters
 adr1 = address
-print "IP address is",adr1
 #split out the ip address, whitespace is the delimiter
 a,b,c,d=adr1.split()
-print a
-print b
-print c
-print d
 #remove the characters in front of the IP address numbers
 adr1=b[5:]
 #split out the four components of the ip address, '.' is the delimiter
 adr1.split('.')
 a,b,c,d=adr1.split('.')
-print "IP address is :", a
-print "IP address is :", b
-print "IP address is :", c
-print "IP address is :", d
 a = int(a)
 b = int(b)
 c = int(c)
 d = int(d)
+print "IP address is :", a, b, c, d
 
 # paus is a function that pauses between each number and
 # displays "dot" so you can tell if the IP address has two numbers the same
