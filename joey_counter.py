e3aea36fe6d80c6c546a575ae0748171ac3ddf01
David Meiklejohn <david@gooligum.com.au>
2016-04-25 14:03:02 +1000

    implemented debounce count

--- joey_counter.py
+++ joey_counter.py
@@ -6,7 +6,7 @@
 #
 # Count resets when JP2 is closed
 #
-#   v1.0    24/04/16
+#   v1.1    24/04/16
 #
 #   David Meiklejohn
 #   Gooligum Electronics
@@ -24,20 +24,34 @@ display.writeInt(count)
 
 try:
   while True:
-    # wait for JP3 to open
-    while display.getJumpers() & 4 == 4:
+    # wait for JP3 to remain open
+    db_cnt = 0
+    while db_cnt < 10:
       if display.getJumpers() & 2:  # reset count if JP2 closed
         count = -1
         break
-      time.sleep(0.2)               # 200ms debounce delay
+      # increment debounce count while JP3 is open
+      if display.getJumpers() & 4 == 0:
+        db_cnt += 1
+      else:
+        db_cnt = 0
+      time.sleep(0.01)                 # debounce delay
+
     # wait for JP3 to close
-    while display.getJumpers() & 4 == 0:
+    db_cnt = 0
+    while db_cnt < 2:
       if display.getJumpers() & 2:  # reset count if JP2 closed
         count = -1
         break
-      time.sleep(0.2)               # 200ms debounce delay
+      # increment debounce count while JP3 is closed
+      if display.getJumpers() & 4 == 4:
+        db_cnt += 1
+      else:
+        db_cnt = 0
+      time.sleep(0.01)                 # debounce delay
+
+    # increment and display count
     count += 1
-    # display count
     display.writeInt(count)   
 
 except KeyboardInterrupt:
